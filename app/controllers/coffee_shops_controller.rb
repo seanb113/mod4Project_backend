6ba6238fa62d30ae4a4adf531cf77e0e71a57fca
Marija Stojanovic <marijastojanovic5@yahoo.com>
2020-02-29 14:58:28 -0500

    second-commitlist and cards are rendered no user or favorites yet...

--- app/controllers/coffee_shops_controller.rb
+++ app/controllers/coffee_shops_controller.rb
@@ -1,12 +1,10 @@
 class CoffeeShopsController < ApplicationController
   def index
     all_shops = CoffeeShop.all
-    render json: all_shops.to_json(
-      :include => {:users =>
-    {:except => [:created_at, :updated_at]}}
-    )
+    render json: all_shops.to_json(           
+      # :include => {:users =>         
+    {:except => [:created_at, :updated_at]})
   end
 
-  def create
-  end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+    
+    def index
+    all_users = User.all
+    render json: all_users.to_json(          
+    {:except => [:created_at, :updated_at]})
+    end
 end
