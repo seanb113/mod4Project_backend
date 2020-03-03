e5dca0776fd341bff28d522aa6cc8bff332cf031
Marija Stojanovic <marijastojanovic5@yahoo.com>
2020-03-03 10:32:26 -0500

    d

--- db/schema.rb
+++ db/schema.rb
@@ -40,14 +40,6 @@ ActiveRecord::Schema.define(version: 2020_03_02_172308) do
   create_table "locations", force: :cascade do |t|
     t.decimal "latitude"
     t.decimal "longitude"
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "places", force: :cascade do |t|
-    t.decimal "latitude"
-    t.decimal "longitude"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
