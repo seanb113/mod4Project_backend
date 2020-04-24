d2111a20af1f85ff6a3ac937c61bd8ce3ee67c94
Sean Beach <sean.beach1@yahoo.com>
2020-04-24 17:08:59 -0400

    bio

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_09_122144) do
+ActiveRecord::Schema.define(version: 2020_04_23_210142) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2020_04_09_122144) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.string "profile_pic", default: "https://pixy.org/src/476/4764586.png"
+    t.text "bio"
   end
 
 end
